23dad32f4281b958145acfdc54d83e3655a51279
flx <1677125841@qq.com>
2025-11-07 10:43:26 +0800

    feat: 使用promise封装异步加载图片方法

---
+++ Promise异步加载图片.js
@@ -0,0 +1,8 @@
+function asyncLoadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error('Failed to load image at ' + url));
+    img.src = url;
+  });
+}
